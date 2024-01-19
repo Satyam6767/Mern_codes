509020ca488dff04947c68c3c01ae309eecb510e
Satyam6767 <satyamkumar6767@gmail.com>
2024-01-19 20:54:00 +0530

    initial commit

---
+++ ecommerce v1/models/Product.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+//schema
+
+const productSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        trim: true,
+        required:true
+    },
+    img:{
+        type:String,
+        trim: true
+    },
+    price:{
+        type:Number,
+        min:0,
+        required:true
+    },
+    desc:{
+        type:String,
+        trim:true
+    }
+})
+
+let Product = mongoose.model('Product', productSchema);
+
+
+module.exports = Product;
